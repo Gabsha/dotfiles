13e0b30286a84df64782088e9ec82461b82412c0
Gabriel Chartrand <gabsha.chartrand@gmail.com>
2022-03-16 21:54:38 -0400

    update nifti mime setup

--- itksnap/nifti-mime.sh
+++ itksnap/nifti-mime.sh
@@ -4,8 +4,14 @@
 ln -sf ${PWD}/itksnap.desktop /home/gabriel/.local/share/applications/
 
 # Symlink icon
-ln -sf {$PWD}/brain.svg /home/gabriel/.local/share/icons/
+ln -sf ${PWD}/brain.svg /home/gabriel/.local/share/icons/
 
 # Create mimetype for nifti
-ln -sf ${PWD}/nifti.xml /home/gabriel/.local/share/mime/
+ln -sf ${PWD}/nifti.xml /home/gabriel/.local/share/mime/packages/
 
+
+# After copying this, needs to update mime database with command:
+# sudo update-mime-database ~/.local/share/mime
+# But it says that this path is not in XDG_DATA_HOME... despite being there (wrong shell env?)
+
+# As a tmp solution, copy to /usr/share/mime/packages/nifti.xml and update.
